a395e11faecef8b05260e3b4c311ffdfc4dca8b5
suu3 <ahob789@ewhain.net>
2022-04-15 05:14:06 +0900

    Update movieDetail.tsx

--- src/components/movieDetail.tsx
+++ src/components/movieDetail.tsx
@@ -10,6 +10,7 @@ import {
   AiFillDelete,
 } from "react-icons/ai";
 import { BiCategoryAlt } from "react-icons/bi";
+import { Animate } from "react-simple-animate";
 
 type commentList = {
   [key: string | number]: string;
@@ -44,55 +45,62 @@ const MovieDetail: React.FC = (props) => {
   };
   return (
     <>
-      <li className="movieDetail">
-        <div
-          className="movie_item"
-          style={{
-            background: `no-repeat center/130% url(${movie?.background_image})`,
-            backgroundRepeat: "no-repeat",
-            objectFit: "cover",
-            zIndex: -2,
-            position: "relative",
-          }}
-        >
-          <img alt="poster" src={movie?.medium_cover_image} />
-          <div className="texts">
-            <h2>{movie?.title}</h2>
-            <p className="row">
-              <label className="label">
-                <AiFillStar style={{ marginRight: ".2rem" }} />
-                Rating
-              </label>
-              {movie?.rating}
-            </p>
-            <p className="row">
-              <label className="label">
-                <AiOutlineFieldTime style={{ marginRight: ".2rem" }} />
-                Running Time
-              </label>
-              {movie?.runtime}
-            </p>
-            <div className="row">
-              <label className="label">
-                <BiCategoryAlt style={{ marginRight: ".2rem" }} />
-                Genres
-              </label>
-              <ul>
-                {movie?.genres.map((genre: string, index: number) => (
-                  <li key={index}>{genre}</li>
-                ))}
-              </ul>
+      <Animate
+        duration={0.3}
+        play={true}
+        end={{ opacity: 1, filter: "blur(0)" }}
+        start={{ opacity: 0, filter: "blur(10px)" }}
+      >
+        <li className="movieDetail">
+          <div
+            className="movie_item"
+            style={{
+              background: `no-repeat center/130% url(${movie?.background_image})`,
+              backgroundRepeat: "no-repeat",
+              objectFit: "cover",
+              zIndex: -2,
+              position: "relative",
+            }}
+          >
+            <img alt="poster" src={movie?.medium_cover_image} />
+            <div className="texts">
+              <h2>{movie?.title}</h2>
+              <p className="row">
+                <label className="label">
+                  <AiFillStar style={{ marginRight: ".2rem" }} />
+                  Rating
+                </label>
+                {movie?.rating}
+              </p>
+              <p className="row">
+                <label className="label">
+                  <AiOutlineFieldTime style={{ marginRight: ".2rem" }} />
+                  Running Time
+                </label>
+                {movie?.runtime}
+              </p>
+              <div className="row">
+                <label className="label">
+                  <BiCategoryAlt style={{ marginRight: ".2rem" }} />
+                  Genres
+                </label>
+                <ul>
+                  {movie?.genres.map((genre: string, index: number) => (
+                    <li key={index}>{genre}</li>
+                  ))}
+                </ul>
+              </div>
+              <p className="row">
+                <label className="label">
+                  <AiOutlineFileText style={{ marginRight: ".2rem" }} />
+                  Description
+                </label>
+              </p>
+              <p className="row">{movie?.description_full}</p>
             </div>
-            <p className="row">
-              <label className="label">
-                <AiOutlineFileText style={{ marginRight: ".2rem" }} />
-                Description
-              </label>
-            </p>
-            <p className="row">{movie?.description_full}</p>
           </div>
-        </div>
-      </li>
+        </li>
+      </Animate>
       <div className="comment">
         <div className="comment-title">Comment</div>
         <div className="input-row">
